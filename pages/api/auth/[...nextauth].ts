f57b6016d96385bcacd8b9dc9f79f94aad7ba54d
Anderson <omorogbeandersonsimeon@gmail.com>
2023-10-19 10:11:12 +0100

    new update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,68 +1,76 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import Credentials from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { compare } from 'bcrypt';
-import prismadb from '@/libs/prismadb';
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { compare } from "bcrypt"
 
-export const authOptions: AuthOptions = {
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+import prismadb from "../../../libs/prismadb"
+
+export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID || "b6988cdf4094d5247201",
+      clientSecret: process.env.GITHUB_SECRET || "d678c7912c5c93024405d3846f2b7dcd7bd1b3dd"
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.GOOGLE_CLIENT_ID || "578631044047-gjl35konse4c41tn3171mq29hnkoh6os.apps.googleusercontent.com",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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"
     }),
     Credentials({
-      id: 'credentials',
-      name: 'Credentials',
+      id: "credentials",
+      name: "Credentials",
       credentials: {
         email: {
-          label: 'Email',
-          type: 'text',
+          label: "Email",
+          type: "text",
         },
-        password: {
-          label: 'Password',
-          type: 'passord'
-        }
+        paswword: {
+          labe: "Password",
+          type: "password",
+        },
       },
+
       async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Email and password required');
+        if (!credentials?.email || !credentials?.paswword) {
+          throw new Error("Email and password required")
         }
 
-        const user = await prismadb.user.findUnique({ where: {
-          email: credentials.email
-        }});
+        const user = await prismadb?.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        })
 
-        if (!user || !user.hashedPassword) {
-          throw new Error('Email does not exist');
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Email does not exist")
         }
 
-        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+        const isCorrectPassword = await compare(
+          credentials?.paswword,
+          user?.hashedPassword
+        )
 
         if (!isCorrectPassword) {
-          throw new Error('Incorrect password');
+          throw new Error("Incorrect Password")
         }
 
         return user;
-      }
-    })
+      },
+    }),
   ],
   pages: {
-    signIn: '/auth'
+    signIn: "/auth",
   },
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development",
   adapter: PrismaAdapter(prismadb),
-  session: { strategy: 'jwt' },
+  session: {
+    strategy: "jwt",
+  },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret: process.env.NEXTAUTH_SECRET
-};
-
-export default NextAuth(authOptions);
-
+  secret: process.env.NEXTAUTH_SECRET,
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,13 +34,7 @@ const Home = () => {
 
   return (
     <>
-      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
-      <Billboard />
-      <div className="pb-40">
-        <MovieList title="Trending Now" data={movies} />
-        <MovieList title="My List" data={favorites} />
-      </div>
     </>
   )
 }
